5b28d501470e4d70195f665e9a7c7abf53087f5b
Saja Hammad <saja3bh@gmail.com>
2025-04-16 05:27:53 +0300

    Implement notifications feature

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Loader } from "@/components/Loader";
+import NotificationItem from "@/components/notification";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/notification.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function notifications() {
+  const notifications = useQuery(api.notifications.getNotifications);
+
+  if (notifications === undefined) return <Loader />;
+  if (notifications.length == 0) return <NoNotificationsFound />;
   return (
-    <View>
-      <Text>notifications</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>notifications screen</Text>
+      </View>
+      <FlatList
+        data={notifications}
+        renderItem={({ item }) => <NotificationItem notification={item} />}
+        keyExtractor={(item) => item?._id?.toString() || ""}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
-  )
+  );
+}
+
+function NoNotificationsFound() {
+  return (
+    <View style={[styles.container, styles.centered]}>
+      <Ionicons name="notifications-outline" size={48} color={COLORS.primary} />
+      <Text style={{ fontSize: 20, color: COLORS.white }}>
+        No notifications yet
+      </Text>
+    </View>
+  );
 }
--- convex/posts.ts
+++ convex/posts.ts
@@ -155,6 +155,16 @@ export const deletePost = mutation({
     for (const comment of comments) {
       await ctx.db.delete(comment._id);
     }
+
+    // delete associated notification
+    const notifications = await ctx.db
+      .query("notifications")
+      .withIndex("by_post", (q) => q.eq("postId", args.postId))
+      .collect();
+
+    for (const notification of notifications) {
+      await ctx.db.delete(notification._id);
+    }
     // delete associated comments
     const bookmarks = await ctx.db
       .query("bookmarks")
